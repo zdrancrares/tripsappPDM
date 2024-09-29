eb3ceff1359745c325f43231ecef8a1274708c61
Ioan Lazar <ilazar@Ioans-MacBook-Pro.local>
2024-09-29 18:59:17 +0300

    02

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ Change log
 - React components (App, Home), https://reactjs.org/docs/components-and-props.html
 - Ionic React navigation, https://ionicframework.com/docs/react/navigation
 - Ionic pages, https://ionicframework.com/docs/layout/structure
+
+02 Define a functional modular structure
+
+- Remove pages & components root modules
+- Add functional modules (e.g. 'todo')
+- Add pages and components into modules
+- React quick start, https://react.dev/learn
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -32,6 +31,7 @@ import '@ionic/react/css/palettes/dark.system.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import { ItemList } from './todo';
 
 setupIonicReact();
 
@@ -39,12 +39,8 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
+        <Route path="/items" component={ItemList} exact={true}/>
+        <Route exact path="/" render={() => <Redirect to="/items"/>}/>
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
